ddfa7b32df70ef1da9e770af1dc5084e021b94f8
Juan Felipe Padilla Sepulveda <juan.sepulveda@correounivalle.edu.co>
2019-10-09 13:01:08 -0500

    Add Scotti's Index.

--- R/xlsx2csv.R
+++ R/xlsx2csv.R
@@ -4,6 +4,8 @@ pacman::p_load(readxl, readr)
 
 # Transform uncertainty indexes from .xlsx to .csv ------------------------
 
+## Economic Policy Uncertainty
+
 economic.policy.uncertainty <- read_xlsx(
   "./Data/Input/US_Policy_Uncertainty_Data.xlsx",
   col_names = c("Year", "Month", "EPU-3C", "EPU-News"),
@@ -11,6 +13,8 @@ economic.policy.uncertainty <- read_xlsx(
   skip = 1,
   n_max = 417
 )
+
+## Monetary Policy Uncertainty
 
 monetary.policy.uncertainty.BBD <- read_xlsx(
   "./Data/Input/US_MPU_monthly.xlsx",
@@ -28,6 +32,8 @@ monetary.policy.uncertainty.HRS <- read_xlsx(
   n_max = 391
 )
 
+## Geopolitical Risk
+
 geopolitical.risk.index <- read_xlsx(
   "./Data/Input/gpr_web_latest.xlsx",
   col_names = c("Date", "GPR", "GPT", "GPA"),
@@ -35,6 +41,19 @@ geopolitical.risk.index <- read_xlsx(
   skip = 1,
   n_max = 417
 )
+
+## Survey-Based Macroeconomic Uncertainty
+
+survey.based.uncertainty <- read_xls(
+  "./Data/Input/Scotti_data28june2019.xls",
+  sheet = 4,
+  col_names = c("Date", "SB"),
+  col_types = c("guess", "numeric", rep("skip", 4)),
+  skip = 1,
+  n_max = 5889
+)
+
+## Econometric Measures of Uncertainty
 
 macro.uncertainty.JLN <- read_xlsx(
   "./Data/Input/MacroUncertaintyToCirculate.xlsx",
@@ -64,6 +83,7 @@ write_csv(economic.policy.uncertainty, "./Data/Input/EPU.csv")
 write_csv(monetary.policy.uncertainty.BBD, "./Data/Input/MPU-BBD.csv")
 write_csv(monetary.policy.uncertainty.HRS, "./Data/Input/MPU-HRS.csv")
 write_csv(geopolitical.risk.index, "./Data/Input/GeopoliticalRiskIndex.csv")
+write_csv(survey.based.uncertainty, "./Data/Input/SurveyBaseUncertainty.csv")
 write_csv(macro.uncertainty.JLN, "./Data/Input/MacroUncertaintyJLN.csv")
-write_csv(real.uncertainty.JLN, "./Data/Input/RealUncertainty.csv")
+write_csv(real.uncertainty.JLN, "./Data/Input/RealUncertaintyJLN.csv")
 write_csv(financial.uncertainty.JLN, "./Data/Input/FinancialUncertaintyJLN.csv")
